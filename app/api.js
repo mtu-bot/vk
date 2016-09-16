a83b64151d3140f9ab7ce94eb708ac072106055c
Alex <me@codebreak.ru>
2016-09-16 23:30:26 +0000

    API part done

--- app/api.js
+++ app/api.js
@@ -37,7 +37,7 @@ function wakeup() {
 		apiRequest('messages.getLongPollServer', utils.urify({'need_pts':1}), startLongpoll);
 	} else {
 		// now we have all data for lp. yay +_*
-		prefs.vk_ts = 1854433975;
+		// prefs.vk_ts = 1854433975;
 		longpoll(prefs.vk_server, prefs.vk_key, prefs.vk_ts);
 	}
 }
@@ -51,16 +51,72 @@ function longpoll(server, key, ts) {
 	  	console.log(body) // @todo: remove this line!
 	  	
 	  	if (body.hasOwnProperty('failed')) {
-	  		console.log("msg limit");
+	  		// seems like longpoll's data has already died, now we need to use longPollHistory..
+	  		apiRequest('messages.getLongPollHistory', utils.urify({'ts':prefs.vk_ts, 'pts':prefs.vk_pts}), longpollHistory);
+	  	} else {
+	  		// longpoll still alive! Lets fetch it's data now!1
+	  		updates = body.updates;
+	  		for (i=0; i<updates.length; i++) {
+	  			update = updates[i];
+	  			console.log(update);
+	  			if (update[0] == 4 && update[2] != 3) {
+	  				sendMessage(update[3], 'pong');
+	  			}
+	  			//sendMessage(uid, 'pong');
+	  		}
+	  		longpoll(server, key, body.ts);
 	  	}
-	  	//
-	  	longpoll(server, key, body.ts);
 
 	  	prefs.vk_ts = body.ts;
 	  	prefs.vk_server = server;
 	  	prefs.vk_key = key;
 	  }
 	})
+}
+
+function longpollHistory(resp) {
+	sorryUIDs = [];
+	response = resp.response;
+	if (response.hasOwnProperty('history')) {
+		messages = response.messages.items;
+		for (i=0; i<messages.length; i++) {
+			message = messages[i];
+			if (message.out == 0) {
+				sorryUIDs.push(message.user_id);
+			}
+		}
+
+		console.log("ES 6 test");
+
+
+		console.log(sorryUIDs)
+
+		sorryUIDsUniq = Array.from(new Set(sorryUIDs));
+
+		console.log(sorryUIDsUniq)
+
+		for(i=0; i<sorryUIDsUniq.length; i++) {
+			sendMessage(sorryUIDsUniq[i], 'Бот немного приболел и был недоступен.. Но! Теперь он снова здоров и готов ответить на все интересующие тебя вопросы. Спрашивай :)');
+		}
+
+		//if (response.more == 1)
+		prefs.vk_pts = response.new_pts;
+		if (response.more == 1) {
+			apiRequest('messages.getLongPollHistory', utils.urify({'ts':prefs.vk_ts, 'pts':response.new_pts}), longpollHistory);
+		} else {
+			apiRequest('messages.getLongPollServer', utils.urify({'need_pts':1}), startLongpoll);
+		}
+	}
+}
+
+/* send message */
+
+function sendMessage(uid, msg) {
+	apiRequest('messages.send', utils.urify({'user_id':uid, 'message':msg}), sendMessageCallback);
+}
+
+function sendMessageCallback(resp) {
+	console.log(resp);
 }
 
 /* needle callbacks */
@@ -76,7 +132,8 @@ function apiRequest(method, params, callback) {
 	  	body = JSON.parse(body);
 	  	console.log(body)
 	  	if (body.hasOwnProperty('error')) {
-	  		//console.log(body);
+	  		console.log("[apiRequest]:");
+	  		console.log(body)
 	  	} else {
 	  		//console.log(body);
 	  		callback(body);
